fdca0b66ae406f06d04ce93f2adb802ef9179257
Ali <108894573+wenranne@users.noreply.github.com>
2022-11-05 15:37:13 +0100

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,14 @@
+#questions(input)
+a = input(' enter your age: ')
+b = input('enter a color: ')
+c = input(f'enter a number between {a} - 100: ')
+d = input('enter your name: ')
+e = input(f'what do you do?(current job, if you do not work, type here{c}: ')
+ 
+#sentences
+
+MatLab = print(f"{d}, let's say after 10 years, at the age of ", int(a) + 10, f"you will be a millionare, by the way, it won't be related to {e}, it will be directly related with the color {b} ")
+
+
+print(MatLab)
+
